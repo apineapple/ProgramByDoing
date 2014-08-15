5cc464a16ff5b1e548837aeca809a39a08c6ed8f
Nick Azua <nazua@zappos.com>
2014-08-15 13:38:01 -0700

    Moved end of round win logic to a function

--- Project/Step 2/Blackjack.java
+++ Project/Step 2/Blackjack.java
@@ -12,6 +12,8 @@ public class Blackjack{
 		Boolean replay = true;
 		Card c;
 		int user_total, dealer_total;
+		int bet_multiplier;
+
 		
 		System.out.println( "Welcome to Blackjack\n" );
 // 		System.out.print( "What is your name? \n> " );
@@ -23,14 +25,21 @@ public class Blackjack{
 		user_total = user.findTotal();
 		dealer_total = dealer.findTotal();
 		
+		// NOT DRY CODE YET until replay loop finished
 		if ( checkForBJ(user_total) && ! checkForBJ(dealer_total) ) {
 			System.out.println( "You got Blackjack!" );
+			bet_multiplier = checkForWin( user_total, dealer_total );	
+			System.out.println ( bet_multiplier );	
 			System.exit(0);
 		} else if ( checkForBJ(user_total) && checkForBJ(dealer_total) ) {
-			System.out.println( "You both got Blackjack." );
+			System.out.println( "You both got Blackjack." );		
+			bet_multiplier = checkForWin( user_total, dealer_total );
+			System.out.println ( bet_multiplier );
 			System.exit(0);
 		} else if ( checkForBJ(dealer_total) ) {
-			System.out.println( "Dealer got Blackjack" );
+			System.out.println( "Dealer got Blackjack" );		
+			bet_multiplier = checkForWin( user_total, dealer_total );
+			System.out.println ( bet_multiplier );
 			System.exit(0);
 		}
 	
@@ -44,7 +53,7 @@ public class Blackjack{
 		
 			if ( choice.equals("hit") ) {
 				c = d.dealCard();
-				System.out.println( "You drew a " + c );
+				System.out.println( "\nYou drew a " + c + ".\n" );
 				user.hand.add(c);
 				user_total = user.findTotal();					
 			} 
@@ -52,13 +61,13 @@ public class Blackjack{
 		}
 	
 		
-		System.out.println( "\n" + user.name + " ended the round with " + user_total + ".\n" );
+		System.out.println( user.name + " ended the round with " + user_total + ".\n" );
 
 		// dealer's turn
 		// dealer will only play if user has not busted			
 		if ( user_total <= 21 ) {
 			choice = "";
-			System.out.println( "*****************\n** " + dealer.name + "'s turn **\n*****************\n" );
+			System.out.println( "*******************\n** " + dealer.name + "'s turn **\n*******************\n" );
 			while ( dealer_total <= 21 && ! choice.equals("stay") ) {
 				// Dealer will hit until it has 17 or greater
 				dealer.showHandAndTotal();
@@ -66,9 +75,8 @@ public class Blackjack{
 				// if ( dealer_total < 17 || (dealer_total < user_total && user_total <= 21) ) { // aggressive dealer
 				if ( dealer_total < 17 ) {
 					choice = "hit";
-					System.out.println( "Dealer chooses to hit." );
 					c = d.dealCard();
-					System.out.println( "He draws a " + c + "." );
+					System.out.println( "\nDealer chooses to hit.\nHe draws a " + c + ".\n" );
 					dealer.hand.add(c);
 					dealer_total = dealer.findTotal();
 
@@ -76,39 +84,17 @@ public class Blackjack{
 				else
 				{
 					choice = "stay";
-					System.out.println( "Dealer stays." );
+					System.out.println( "Dealer stays.\n" );
 				}
 			}
 		}
 					
-		System.out.println( "\n" + dealer.name + " ended the round with " + dealer_total + ".\n" );
-
-		if ( user_total <= 21 )
-		{
-			if ( dealer_total <= 21 )
-			{
-				if (user_total > dealer_total )
-				{
-					System.out.println( "You beat Dealer!" );
-				}
-				else if ( user_total == dealer_total )
-				{
-					System.out.println( "You Drew." );
-				}
-				else
-				{
-					System.out.println( "Dealer beat you" );
-				}
-			}
-			else
-			{
-				System.out.println( "Dealer busted!" );
-			}
-		}
-		else
-		{
-			System.out.println( "you busted" );
-		}
+		System.out.println( dealer.name + " ended the round with " + dealer_total + ".\n" );
+		
+		bet_multiplier = checkForWin( user_total, dealer_total );
+		System.out.println( bet_multiplier );
+		
+		
 
 
 		
@@ -124,30 +110,53 @@ public class Blackjack{
 		d.shuffle();
 		
 // Testing win logic
-// 			dealer.hand.add(new Card("8", "S", 8));
-// 			dealer.hand.add(new Card("J", "S", 10));
-// 			p.hand.add(new Card("9", "S", 9));
-// 			p.hand.add(new Card("J", "S", 10));
+			dealer.hand.add(new Card("A", "S", 10));
+			dealer.hand.add(new Card("J", "S", 10));
+			p.hand.add(new Card("A", "S", 11));
+			p.hand.add(new Card("J", "S", 10));
 
 		// Deals 2 cards from top of deck to the players
-		for (i = 0; i < 2; i++ )
-			dealer.hand.add(d.dealCard());
-	
-		for (i = 0; i < 2; i++ )
-			p.hand.add(d.dealCard());
+// 		for (i = 0; i < 2; i++ )
+// 			dealer.hand.add(d.dealCard()); 	
+// 		for (i = 0; i < 2; i++ )
+// 			p.hand.add(d.dealCard());
 
 		p.showHandAndTotal();
-		System.out.println( dealer.name + " has [" + dealer.hand.get(0) + ", **]" );
-		System.out.println( dealer.name + "'s total is hidden." );
-		dealer.showHandAndTotal();
-		System.out.println( "Did you get blackjack: " + checkForBJ(p.findTotal()) );
-		System.out.println( "Did the dealer: " + checkForBJ(dealer.findTotal()) );
+		System.out.println( "\n=-=-=-=-=-=-=-=-=-=-=--=-=\n" + dealer.name + " has [" + dealer.hand.get(0) + ", **]" );
+		System.out.println( dealer.name + "'s total is hidden.\n" );
+// Testing BJ game logic
+// 		dealer.showHandAndTotal();
+// 		System.out.println( "Did you get blackjack: " + checkForBJ(p.findTotal()) );
+// 		System.out.println( "Did the dealer: " + checkForBJ(dealer.findTotal()) );
 
 			
 	}
 	
 	public static Boolean checkForBJ(int score) {	
 		return (  score == 21 );
+	}
+	
+	public static int checkForWin(int u_total, int d_total) {
+		if ( u_total <= 21 ) {
+			if ( d_total <= 21 ) {
+				if ( u_total > d_total ) {
+					System.out.println( "You beat Dealer!\n" );
+					return 1;
+				} else if ( u_total == d_total ) {
+					System.out.println( "You Drew.\n" );
+					return 0;
+				} else {
+					System.out.println( "Dealer beat you\n" );
+					return -1;
+				}
+			} else {
+				System.out.println( "Dealer busted! You win!!\n" );
+				return 1;
+			}
+		} else {
+			System.out.println( "you busted\n" );
+			return -1;
+		}
 	}
 	
 
