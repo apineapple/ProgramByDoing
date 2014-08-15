1a2f4659e2fcbb5c36cc4066e1fb25e8e9415b7e
Nick Azua <nazua@zappos.com>
2014-08-15 15:19:14 -0700

    Formated display output.

--- Project/Step 2/Blackjack.java
+++ Project/Step 2/Blackjack.java
@@ -13,7 +13,7 @@ public class Blackjack{
 		Boolean replay = true;
 		Card c;
 		int user_total, dealer_total;
-		int bet_multiplier;
+		int pay_out_multiplier;
 		
 		
 		System.out.println( "Welcome to Blackjack\n" );
@@ -21,19 +21,22 @@ public class Blackjack{
 // 		user_name = kb.nextLine();
 		BlackjackPlayer user = new BlackjackPlayer(user_name);
 		
-		initializeGame(d, user, dealer);
+		initializeRound(d, user, dealer);
 		
 		user_total = user.findTotal();
 		dealer_total = dealer.findTotal();
 		
 		// Check to see if either player received Blackjack		
-		bet_multiplier = checkForBJ(user_total, dealer_total);
-		if ( bet_multiplier != 2 ) {
+		pay_out_multiplier = checkForBJ(user_total, dealer_total);
+		if ( pay_out_multiplier != 2 ) {
 			dealer.showHandAndTotal();
 			d = finishRound( d, user, dealer );
+			System.out.println ( pay_out_multiplier );
+			// testing reloading deck			
+// 			System.out.println( "Deck d: " + d.cards + "\nSize: " + d.cards.size() );
 			System.exit(0);
 		} else {
-			bet_multiplier = 0;
+			pay_out_multiplier = 0;
 		}
 			
 	
@@ -53,9 +56,9 @@ public class Blackjack{
 			} 
 			
 		}
-	
 		
-		System.out.println( user.name + " ended the round with " + user_total + ".\n" );
+		
+		System.out.println( "\n" + user.name + " ended the round with " + user_total + ".\n" );
 
 		// dealer's turn
 		// dealer will only play if user has not busted			
@@ -65,15 +68,16 @@ public class Blackjack{
 			while ( dealer_total <= 21 && ! choice.equals("stay") ) {
 				// Dealer will hit until it has 17 or greater
 				dealer.showHandAndTotal();
-				
-				// if ( dealer_total < 17 || (dealer_total < user_total && user_total <= 21) ) { // aggressive dealer
 				if ( dealer_total < 17 ) {
-					choice = "hit";
-					c = d.dealCard();
-					System.out.println( "\nDealer chooses to hit.\nHe draws a " + c + ".\n" );
-					dealer.hand.add(c);
-					dealer_total = dealer.findTotal();
-
+					if ( dealer_total <= user_total && user_total <= 21 ) { 
+						choice = "hit";
+						c = d.dealCard();
+						System.out.println( "\nDealer chooses to hit.\nHe draws a " + c + ".\n" );
+						dealer.hand.add(c);
+						dealer_total = dealer.findTotal();
+					} else {
+						choice = "stay";
+					}
 				}
 				else
 				{
@@ -82,31 +86,32 @@ public class Blackjack{
 				}
 			}
 		}
-					
+
 		System.out.println( dealer.name + " ended the round with " + dealer_total + ".\n" );
-		
+		System.out.println( "******************\n** End of Round **\n******************\n" );
+
 		// Determine the winner and reload the deck
-		bet_multiplier = checkForWin( user_total, dealer_total );
-		System.out.println( bet_multiplier );
+		pay_out_multiplier = checkForWin( user_total, dealer_total );
+		System.out.println( pay_out_multiplier );
 		d = finishRound( d, user, dealer );
-		System.out.println ( d.cards + "\ndeck size: " + d.cards.size() );	
-		
+// testing reloading deck			
+// 		System.out.println( "Deck d: " + d.cards + "\nSize: " + d.cards.size() );
 
-
-		
-		
 	}
+	
+	
+	
 	/** 
 	 * Shuffles the deck and deals two cards to each player
 	 *
 	 */
-	public static void initializeGame( Deck d, BlackjackPlayer p, BlackjackPlayer dealer ) {
+	public static void initializeRound( Deck d, BlackjackPlayer p, BlackjackPlayer dealer ) {
 		int i;
 		
 		d.shuffle();
 		
 // Testing win logic
-// 			dealer.hand.add(new Card("A", "S", 11));
+// 			dealer.hand.add(new Card("A", "S", 10));
 // 			dealer.hand.add(new Card("J", "S", 10));
 // 			p.hand.add(new Card("A", "S", 11));
 // 			p.hand.add(new Card("J", "S", 10));
@@ -134,12 +139,15 @@ public class Blackjack{
 	
 	public static int checkForBJ(int u_total, int d_total) {	
 		if ( u_total == 21 && d_total != 21 ) {
+			System.out.println( "*******************\n** End of Round **\n*******************\n" );
 			System.out.println( "You got Blackjack! You win this round.\n" );
 			return 1;
 		} else if ( u_total == 21 && d_total == 21 ) {
+			System.out.println( "*******************\n** End of Round **\n*******************\n" );
 			System.out.println( "You both got Blackjack. The round is a draw.\n" );		
 			return 0;
 		} else if ( d_total == 21 ) {
+			System.out.println( "*******************\n** End of Round **\n*******************\n" );
 			System.out.println( "Dealer got Blackjack. You lose this round.\n" );		
 			return -1;
 		} else {
