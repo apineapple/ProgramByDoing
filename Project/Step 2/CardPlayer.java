f58a0107af9924314a0f4bb04d64c5ec2e00daf9
Nick Azua <nazua@zappos.com>
2014-08-16 09:28:49 -0700

    Added drawCard and discardCard functions

--- Project/Step 2/CardPlayer.java
+++ Project/Step 2/CardPlayer.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class CardPlayer {
 	ArrayList<Card> hand = new ArrayList<Card>();
@@ -12,29 +13,58 @@ public class CardPlayer {
 				
 		return old_hand;
 	}
+	
+	// other possible functions draw card, discard card
+	
+	public Card drawCard( Deck d ) {
+		Card c;
+		
+		c = d.dealCard();
+		this.hand.add(c);
+		
+		return c;
+	}
+	
+	public Card discardCard(int card_num) {
+		Card c;
+		
+		c = this.hand.remove(card_num - 1);
+		
+		return c;
+	}
 
 // 	public static void main( String[] args ) {
 // 		CardPlayer p1 = new CardPlayer();
 // 		Deck d = new Deck(1);
 // 		Card c;
+// 		Scanner kb = new Scanner(System.in);
+// 		int discard;
 // 		
 // 		d.shuffle();
 // 	
+// 		// Testing drawCard()
 // 		for (int i = 0; i < 5; i++ ) {
-// 			c = d.dealCard();		
+// 			System.out.println( "Start of loop Player hand: " + p1.hand );
+// 			c = p1.drawCard(d);		
 // 			System.out.println(c);
-// 			p1.hand.add(c);
-// 			System.out.println( "Player hand: " + p1.hand );
+// 			System.out.println( "End of loop Player hand: " + p1.hand );
 // 		}
+// 		// Testing discardCard()
+// 		System.out.println ( "Discard from your hand \n" + p1.hand + "\n[1   2   3   4   5 ]" ); 
+// 		discard = kb.nextInt();
 // 		
-// Testing clearHand
+// 		c = p1.discardCard(discard);
+// 		System.out.println(c);
+// 		System.out.println( "Player hand after discard: " + p1.hand );
+// 		
+// // Testing clearHand
 // 		ArrayList<Card> return_to_deck = new ArrayList<Card>();
 // 		return_to_deck = p1.clearHand();
 // 		System.out.println ( "Calling .clearHand()" );
 // 		System.out.println ( "Player hand: " + p1.hand );
 // 		System.out.println ( "Cards to return: " + return_to_deck );
 // 		
-// Testing returning card to deck
+// // Testing returning card to deck
 // 		System.out.println( "Deck\n" + d.cards + "\nwith " + d.cards.size() );
 // 		while ( return_to_deck.size() > 0 ) {
 // 			c = return_to_deck.remove(0);
